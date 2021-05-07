59674569b4f1a32e59cffbc05b3348aee016f62b
hnhaefliger <henry_haefliger@protonmail.com>
2021-05-08 01:56:08 +0200

    last attempt tonight to fix the issues

--- crowdbyte/crowdbyte/asgi.py
+++ crowdbyte/crowdbyte/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import rooms.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crowdbyte.crowdbyte.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crowdbyte.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
