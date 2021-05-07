1e951a315a3ac5b7c80fb961a89cd4fe53b6ce3f
hnhaefliger <henry_haefliger@protonmail.com>
2021-05-08 01:51:23 +0200

    last attempt tonight to fix the issues

--- crowdbyte/manage.py
+++ crowdbyte/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowdbyte.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowdfi.crowdbyte.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
