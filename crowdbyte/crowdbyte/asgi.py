b2d248e30aea7b24cbb9ea49c4b465f05a0ef5d6
hnhaefliger <henry_haefliger@protonmail.com>
2021-05-08 21:18:12 +0200

    fuck heroku

--- crowdbyte/crowdbyte/asgi.py
+++ crowdbyte/crowdbyte/asgi.py
@@ -5,9 +5,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import rooms.routing
 
-import django
-django.setup()
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crowdbyte.settings")
 
 application = ProtocolTypeRouter({
