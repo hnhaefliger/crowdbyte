93ff79767b511872da506754eaf232ec99dd3652
hnhaefliger <henry_haefliger@protonmail.com>
2021-05-08 22:26:51 +0200

    fuck heroku

--- crowdbyte/crowdbyte/settings.py
+++ crowdbyte/crowdbyte/settings.py
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_HOST', 'localhost'), 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
