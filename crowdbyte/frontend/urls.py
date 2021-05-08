c77605fb374ae1c8ef882545eec2ca80e9f6d96d
hnhaefliger <henry_haefliger@protonmail.com>
2021-05-09 01:10:34 +0200

    cert

--- crowdbyte/frontend/urls.py
+++ crowdbyte/frontend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('room/<str:room_id>', views.room, name='room'),
     path('host', views.start, name='start'),
     path('host/<str:room_id>', views.host, name='host'),
+    path('.well-known/acme-challenge/bPBXnTTIHP-0dTFV52yTu1KtUfZ1nBtg-Lp5uMkY-0M', views.cert, name='cert')
 ]
--- crowdbyte/frontend/views.py
+++ crowdbyte/frontend/views.py
@@ -15,3 +15,6 @@ def host(request, room_id):
     return render(request, 'frontend/host.html', {
         'room_id': room_id
     })
+
+def cert(request):
+    return 'bPBXnTTIHP-0dTFV52yTu1KtUfZ1nBtg-Lp5uMkY-0M.gDZ5atBUq--CVc08u9jv_1_j8_bRh62ypuVIS1XgapU'
